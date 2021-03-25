016e990bb9d4547c608b607c2969286b934a3045
Irvin Buendia <irvin.buendia1@gmail.com>
2021-03-25 17:03:50 -0500

    Completed do while excercise

--- js/while.js
+++ js/while.js
@@ -18,8 +18,50 @@
 // 32768
 // 65536
 
-var output = 1;
-while (output <= 65535) {
-    output = output * 2;
+var output = 2;
+while (output <= 65536) {
     console.log(output);
+    output = output * 2;
 }
+
+
+// var num = prompt("What number do you want to start with?");
+// var originalNumber= num;
+// var exponent = 2;
+//
+// while (exponent < 16) {
+//     num = num * originalNumber;
+//     console.log(originalNumber + " to the power of " + exponent + " = " + num);
+//     exponent = exponent + 1
+// }
+
+
+
+// An ice cream seller can't go home until she sells all of her cones.
+// First write enough code that generates a random number between 50 and 100 representing the amount of cones to sell before you start your loop. Inside of the loop your code should generate another random number between 1 and 5, simulating the amount of cones being bought by her clients. Use a do-while loop to log to the console the amount of cones sold to each person. This is a way get the random numbers for this exercise.
+//
+//
+// // This is how you get a random number between 50 and 100
+// var allCones = Math.floor(Math.random() * 50) + 50;
+// // This expression will generate a random number between 1 and 5
+// Math.floor(Math.random() * 5) + 1;
+// The output should be similar to the following:
+//
+// 5 cones sold...  // if there are enough cones
+// Cannot sell you 6 cones I only have 3...  // If there are not enough cones
+// Yay! I sold them all! // If there are no more cones
+
+var allCones = Math.floor(Math.random() * 50) + 50;
+console.log("We have " + allCones + " to sell.")
+
+do {
+    var conesSold = Math.floor(Math.random() * 5) + 1;
+    if (allCones < conesSold) {
+        console.log("Sorry we do not have " + conesSold + ", we only have " + allCones + " left");
+    } else {
+        allCones = allCones - conesSold;
+        console.log(conesSold + " cones sold " + allCones + " cones left");
+    }
+}
+while (allCones > 0) ;
+console.log("We sold all the cones!");
