1f1507b63bc8956db6727bf5fd23c05db290fce4
Irvin Buendia <irvin.buendia1@gmail.com>
2021-06-06 22:12:46 -0500

    reverse a string warmup

--- js/warmups.js
+++ js/warmups.js
@@ -44,3 +44,20 @@ timesFive(20)
 timesFive("five")
 
 const multiplybyFive = num => typeof  num === "number" ? num * 5 : 0;
+
+function reverseString(s) {
+    // Create the result list
+    const result = [];
+    // Start from the end of the string and iterate towards the start
+    for (let i = s.length-1; i >= 0; i -= 1) {
+        // Push the current char in the list
+        result.push(s[i]);
+    }
+    // Combine the result in a string
+    return result.join('');
+}
+
+// Examples
+console.log(reverseString(""))
+console.log(reverseString("abc"))
+console.log(reverseString("aaabbbcccd"))
